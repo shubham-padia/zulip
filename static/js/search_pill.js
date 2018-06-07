88182054e205e761beffdb9e9f0db056b1502408
Shubham Padia <shubhamapadia@gmail.com>
2018-06-07 22:10:25 +0530

    search: Add create_pills to search_pil.js.
    The items for search pill contain 2 keys, diplay_value and search_string.
    display_value is the description of the search string. This description
    is obtained from the operator object by using Filter.describe.
    create_item_from_search_string creates an item in the above format given
    an operator. get_search_string_from_item does the reverse of that.
    
    Adding all the operator information i.e the operator, operand and
    negated fields along with the search_string and description was tried out.
    It was dropped because it didn't provide any advantage as one had to
    always calculate the search_string and the description from the operator.

--- static/js/search_pill.js
+++ static/js/search_pill.js
@@ -1,5 +1,28 @@
 var search_pill = (function () {
 var exports = {};
+
+exports.create_item_from_search_string = function (search_string) {
+    var operator = Filter.parse(search_string);
+    var description = Filter.describe(operator);
+    return {
+        display_value: description,
+        search_string: search_string,
+    };
+};
+
+exports.get_search_string_from_item = function (item) {
+    return item.search_string;
+};
+
+exports.create_pills = function (pill_container) {
+    var pills = input_pill.create({
+        container: pill_container,
+        create_item_from_text: exports.create_item_from_search_string,
+        get_text_from_item: exports.get_search_string_from_item,
+    });
+    return pills;
+};
+
 return exports;
 }());
 
