7895373e386cea18b7e586946dd9362c8040bb50
Shubham Padia <shubhamapadia@gmail.com>
2024-05-14 22:29:06 +0530

    test

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+    title: "Zulip Help Center"
+  })]
+});
---
+++ src/content/docs/index.md
@@ -0,0 +1,10 @@
+---
+title: Zulip help center
+description: Learn more about the project I’m working on.
+---
+
+# Zulip help center
+
+Welcome to the [Zulip](/) help center!
+
+{!sidebar_index.md!}
